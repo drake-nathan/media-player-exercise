f71bc2716504b5d650e399162d3fc439dec32d71
Nathan Drake <nathandrakedrums@gmail.com>
2025-04-04 17:40:27 -0500

    chore: Run format/lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-function App() {
-  return "Hello World";
-}
+/* eslint-disable unicorn/filename-case */
+// I prefer kebab case for filenames, but it's not worth dealing with the git case sensitivity :-)
+
+const App = () => "Hello World";
 
 export default App;
